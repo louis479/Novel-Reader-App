aaf9eaa438d1df255f77532210ddb7ed84f94b40
louis-ngugi <louisngugi858@gmail.com>
2025-03-11 15:01:46 +0300

    added one-to-many relationship with many books to one author in models.py

--- models.py
+++ models.py
@@ -10,4 +10,10 @@ class Author(Base):
     name = Column(String, nullable=False)
 
     books = relationship('Book', back_populates='author', cascade="all, delete")
-    
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('authors.id'))
