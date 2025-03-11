96035b4745a815a29bd424d6ca3f09f790565828
louis-ngugi <louisngugi858@gmail.com>
2025-03-11 14:48:52 +0300

    added imports class Author(Base) with id and name content in models.py

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
+Base = declarative_base()
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    
