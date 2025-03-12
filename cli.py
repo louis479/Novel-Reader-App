c80dc19da84589aacacc9c37bc15080f095df5b7
louis-ngugi <louisngugi858@gmail.com>
2025-03-12 23:59:22 +0300

    search for a book in @cli.command

--- cli.py
+++ cli.py
@@ -7,11 +7,25 @@ def cli():
     pass
 
 @cli.command()
-@click.option("", prompt="Book Title", help="Enter the book title")
-@click.option("", prompt="Author", help="Enter the author's name")
-@click.option("", default="", help="Enter the book genre")
-@click.option("", type=int, default=0, help="Number of pages")
+@click.option("--title", prompt="Book Title", help="Enter the book title")
+@click.option("--author", prompt="Author", help="Enter the author's name")
+@click.option("--genre", default="", help="Enter the book genre")
+@click.option("--pages", type=int, default=0, help="Number of pages")
 def add_book(title, author, genre, pages):
     """Add a new book"""
     book = Book.create(title, author, genre, pages)
     click.echo(f"Book '{book.title}' added successfully!")
+
+@cli.command()
+@click.option("--keyword", prompt="Search Keyword", help="Enter name of title or author")
+def search_book(keyword):
+    """search for a book with title or author"""
+    book = Book.search(keyword)
+    if books:
+        for book in books:
+            click.echo(f"{book.id}: {book.title} by {book.author}")
+    else:
+        click.echo("No books found.")
+
+if __name__ == "__main__":
+    cli()
