13ca2256b67aa0d85a5d9d1473281c8e03278826
louis-ngugi <louisngugi858@gmail.com>
2025-03-12 22:19:31 +0300

    Adding a new book in cli.py

--- cli.py
+++ cli.py
@@ -6,3 +6,12 @@ def cli():
     """Novel Reader App"""
     pass
 
+@cli.command()
+@click.option("", prompt="Book Title", help="Enter the book title")
+@click.option("", prompt="Author", help="Enter the author's name")
+@click.option("", default="", help="Enter the book genre")
+@click.option("", type=int, default=0, help="Number of pages")
+def add_book(title, author, genre, pages):
+    """Add a new book"""
+    book = Book.create(title, author, genre, pages)
+    click.echo(f"Book '{book.title}' added successfully!")
